3ac6900d99969509725c916df91c929300e52cb5
Esteban Sánchez <esteban@ticketea.com>
2015-05-27 16:04:27 +0200

    Added commands base file

---
+++ forseti/commands/base.py
@@ -0,0 +1,39 @@
+import inspect
+import importlib
+
+
+from abc import ABCMeta, abstractmethod
+from six import add_metaclass
+
+
+@add_metaclass(ABCMeta)
+class BaseForsetiCommand(object):
+    @abstractmethod
+    def run(self, configuration, cli_arguments):
+        raise NotImplementedError
+
+    @abstractmethod
+    def cli_command(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def cli_command_doc(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def cli_command_options_doc(self):
+        raise NotImplementedError
+
+
+def get_all_commands():
+    module = importlib.import_module("forseti.commands.commands")
+    commands = []
+    for _, obj in inspect.getmembers(module):
+        if (
+            inspect.isclass(obj) and
+            issubclass(obj, BaseForsetiCommand) and
+            not inspect.isabstract(obj)
+        ):
+            commands.append(obj)
+
+    return commands
