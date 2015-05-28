23a7afe24c55a5e7079244b4620e62865411f8cc
Esteban Sánchez <esteban@ticketea.com>
2015-05-28 16:51:08 +0200

    Added metadata, requirements file and improved setup.py

---
+++ forseti/metadata.py
@@ -0,0 +1,7 @@
+__version__ = '0.1.2'
+__uname__ = 'forseti'
+__long_name__ = "Manage your AWS autoscaling groups and policies and create AMIs for autoscaling purposes"
+__author__ = 'ticketea'
+__email__ = 'dev@ticketea.com'
+__url__ = 'http://github.com/ticketea/forseti'
+__license__ = 'BSD'
