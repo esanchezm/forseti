f549cbd2f9ab8668e5785506a3c03ae16bc74131
Esteban Sánchez <esteban@ticketea.com>
2015-07-21 14:43:42 +0200

    Raised forseti version

--- forseti/metadata.py
+++ forseti/metadata.py
@@ -1,4 +1,4 @@
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 __uname__ = 'forseti'
 __long_name__ = "Manage your AWS autoscaling groups and policies and create AMIs for autoscaling purposes"
 __author__ = 'ticketea'
